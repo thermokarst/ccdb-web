60eadf517f8d2e69d0391183f466f76aa0df23b3
Matthew Dillon <matthewrdillon@gmail.com>
2017-12-01 11:55:35 -0700

    ENH: Revised collection details (#54)

--- app/components/collection/detail-container.js
+++ app/components/collection/detail-container.js
@@ -6,10 +6,9 @@ export default Component.extend({
   // ARGS
   model: null,
 
-  columns: [
+  mainColumns: [
     { label: 'Project', valuePath: 'project.name', },
     { label: 'IACUC', valuePath: 'project.iacucNumber', },
-    { label: 'Species', valuePath: 'speciesAndCounts', },
     { label: 'Region', valuePath: 'studyLocation.site.region.name', },
     { label: 'Site', valuePath: 'studyLocation.site.name', },
     { label: 'Study Location', valuePath: 'studyLocation.code', },
@@ -19,5 +18,12 @@ export default Component.extend({
     { label: 'Start', valuePath: 'startDateTime', },
     { label: 'End', valuePath: 'endDateTime', },
     { label: 'ADFG Permit', valuePath: 'adfgPermit.name', },
+  ],
+
+  collectionSpeciesColumns: [
+    { label: 'Species', valuePath: 'species.commonName' },
+    { label: 'Count', valuePath: 'count' },
+    { label: 'Count Estimated?', valuePath: 'countEstimated' },
+    { label: 'Sex', valuePath: 'sex' },
   ],
 });
