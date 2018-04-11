86ab62cb0f634bb7d4320a126bb932bea52d9f82
Matthew Dillon <matthewrdillon@gmail.com>
2018-04-10 20:14:48 -0700

    DEBUG: console statements for everyone! (#84)

--- app/routes/collections/index.js
+++ app/routes/collections/index.js
@@ -43,26 +43,37 @@ export default Route.extend({
 
     const store = this.get('store');
 
+    /* eslint-disable no-console */
+
     let project = controller.get('project');
+    console.log('project', project);
     project = project.map(id => store.peekRecord('project', id));
 
     let region = controller.get('region');
+    console.log('region', region);
     region = region.map(id => store.peekRecord('region', id));
 
     let site = controller.get('site');
+    console.log('site', site);
     site = site.map(id => store.peekRecord('site', id));
 
     let studyLocation = controller.get('study_location');
+    console.log('studyLocation', studyLocation);
     studyLocation = studyLocation.map(id => store.peekRecord('study-location', id));
 
     let collectionMethod = controller.get('collection_method');
+    console.log('collectionMethod', collectionMethod);
     collectionMethod = collectionMethod.map(id => store.peekRecord('collection-method', id));
 
     let adfgPermit = controller.get('adfg_permit');
+    console.log('adfgPermit', adfgPermit);
     adfgPermit = adfgPermit.map(id => store.peekRecord('adfg-permit', id));
 
     let species = controller.get('species');
+    console.log('species', species);
     species = species.map(id => store.peekRecord('species', id));
+
+    /* eslint-enable no-console */
 
     const numberOfTraps = controller.get('number_of_traps');
     const collectionStartDate = controller.get('collection_start_date');
