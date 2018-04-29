22be697ef69bf9fb839a214193c1e5a387d7ff14
Matthew Dillon <matthewrdillon@gmail.com>
2018-04-29 11:58:26 -0700

    MAINT: swap vanilla sentry for ember-cli-sentry (#92)

--- config/environment.js
+++ config/environment.js
@@ -20,12 +20,23 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    sentry: {
+      dsn: 'https://fd3c695fa9394de48a7c69b7a322960b@sentry.io/1186914',
+      globalErrorCatching: false,
+    },
+
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+      'img-src': 'data: app.getsentry.com',
     }
   };
 
   if (environment === 'development') {
     ENV.APP.API_HOST = 'http://localhost:8000';
     ENV.APP.API_NAMESPACE = 'api/v1';
+    ENV.sentry.development = true;
   }
 
   if (environment === 'test') {
